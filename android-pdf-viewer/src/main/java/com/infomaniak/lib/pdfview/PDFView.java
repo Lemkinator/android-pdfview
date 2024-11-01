2a293fe7b235d6619271843b8a6fe1d4a27bccfe
Leonard <leo@leonard-lemke.com>
2024-11-01 01:56:59 +0100

    temp disable maxscale

--- android-pdf-viewer/src/main/java/com/infomaniak/lib/pdfview/PDFView.java
+++ android-pdf-viewer/src/main/java/com/infomaniak/lib/pdfview/PDFView.java
@@ -104,7 +104,7 @@ public class PDFView extends RelativeLayout {
 
     private static final String TAG = PDFView.class.getSimpleName();
 
-    public static final float DEFAULT_MAX_SCALE = 3f;
+    public static final float DEFAULT_MAX_SCALE = 2f; //3f;
     public static final float DEFAULT_MID_SCALE = 2f;
     public static final float DEFAULT_MIN_SCALE = 1f;
 
