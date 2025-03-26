e1b78c70a3d30a1e481f34449727f607d8d44b13
Leonard <leo@leonard-lemke.com>
2025-03-26 23:34:09 +0100

    Upgrade SDK to 36

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "de.lemke.pdfview"
 
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 26
--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 
 android {
     namespace = "de.lemke.pdfview.sample"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
