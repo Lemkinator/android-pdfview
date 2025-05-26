0c49918f341c6afaddba74d475a7542e79980ba1
Leonard <leo@leonard-lemke.com>
2025-05-26 22:41:37 +0200

    Upgrade Kotlin plugin to 2.1.21, set Java and Kotlin compatibility to version 21, and update Gradle wrapper to 8.14.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,5 +21,5 @@ allprojects {
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("com.android.library") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.21" apply false
 }
